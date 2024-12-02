f1940ba7eca34561d66cf0a0feeac6746bfadec3
Hendrik Denniger <hendrik.denniger@adesso.de>
2024-12-02 11:42:33 +0100

    DayO2 done

---
+++ Day02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Text;
+// PuzzleInput02.txt
+var records = LibraryTools.Tools.GetInput("PuzzleInput02.txt");
+var recordsList = records.Select(r=>r.Split(" ").Select(s=>Int32.Parse(s)).ToList()).ToList();
+
+int result = 0;
+bool works = false;
+
+foreach (List<int> record in recordsList)
+{
+    for (int j = 0; j < record.Count; j++)
+    {
+        var recordGutted = record.Where((value,r) => r != j).ToList();
+        
+        if (LibraryTools.Tools.IsDescOrAsc(recordGutted))
+        {
+            for (int i = 1; i < recordGutted.Count; i++)
+            {
+                if (Math.Abs(recordGutted[i] - recordGutted[i - 1]) > 3 || Math.Abs(recordGutted[i] - recordGutted[i - 1]) == 0)
+                {
+                    break; 
+                }
+                if (i == recordGutted.Count - 1) works = true;
+            }
+        }
+
+        if (works)
+        {
+            result++;
+            works = false;
+            break;
+        }
+    }
+   
+    
+}
+Console.WriteLine(result);
---
+++ LibraryTools/Tools.cs
@@ -0,0 +1,18 @@
+﻿namespace LibraryTools;
+
+public class Tools
+{
+    public static String[] GetInput(string filename)
+    {
+        var path = Directory.GetCurrentDirectory();
+        path = Path.GetFullPath(Path.Combine(path,@"..\..\..\", filename)); 
+        
+        var lines = File.ReadAllLines(path);
+        return lines;
+    }
+
+    public static Boolean IsDescOrAsc(List<int> list )
+    {
+        return (list.SequenceEqual(list.OrderByDescending(l => l).ToList())   || list.SequenceEqual(list.OrderBy(l => l).ToList()));
+    } 
+}
