b51e2ccdea798ef5b7c8ac0a1ebca5a98f5ef67a
Hendrik Denniger <hendrik.denniger@adesso.de>
2024-12-05 16:07:57 +0100

    Day05 done!

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -1,12 +1,63 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var input_rules = LibraryTools.Tools.GetInput("trial.txt");
-var input_updates = LibraryTools.Tools.GetInput("trial_part2.txt");
+string trial = "trial.txt";
+string trial2 = "trial_part2.txt";
+string input = "InputDay05.txt";
+string input2 = "InputDay05_part2.txt";
 
-var rules = input_rules.Select(r=>r.Split("|").Select(int.Parse).ToArray()).ToList();
-var updates = input_updates.Select(i => i.Split(",").Select(int.Parse).ToArray()).ToList();
+var inputRules = LibraryTools.Tools.GetInput(input);
+var inputUpdates = LibraryTools.Tools.GetInput(input2);
 
-int result;
+var rules = inputRules.Select(r=>r.Split("|").Select(int.Parse).ToArray()).ToList();
+var updates = inputUpdates.Select(i => i.Split(",").Select(int.Parse).ToArray()).ToList();
+
+int result=0;
+bool ruleViolated = false;
+/*foreach (var update in updates)
+{
+    foreach (var number in update)
+    {
+        foreach (var rule in rules)
+        {
+            var numberPos = Array.IndexOf(rule, number);
+            if (numberPos != -1 && update.Contains(rule[Math.Abs(numberPos-1)]))
+            {
+                if (numberPos == 0)
+                {
+                    if(Array.IndexOf(update, number) > Array.IndexOf(update,rule[Math.Abs(numberPos-1)]))
+                    {
+                        ruleViolated = true;
+                    }
+                }
+                else
+                {
+                    if(Array.IndexOf(update, number) < Array.IndexOf(update,rule[Math.Abs(numberPos-1)]))
+                    {
+                        ruleViolated = true;
+                    }
+                }
+                
+                    
+            }
+        }
+
+        if (ruleViolated)
+        {
+            break;
+        }
+    }
+
+    if (ruleViolated)
+    {
+        ruleViolated = false;
+        continue;
+    }
+    
+    result += update[(update.Length-1)/2];
+}
+
+Console.WriteLine(result);*/
+
 foreach (var update in updates)
 {
     foreach (var number in update)
@@ -16,8 +67,95 @@ foreach (var update in updates)
             var numberPos = Array.IndexOf(rule, number);
             if (numberPos != -1 && update.Contains(rule[Math.Abs(numberPos-1)]))
             {
+                if (numberPos == 0)
+                {
+                    if(Array.IndexOf(update, number) > Array.IndexOf(update,rule[Math.Abs(numberPos-1)]))
+                    {
+                        ruleViolated = true;
+                    }
+                }
+                else
+                {
+                    if(Array.IndexOf(update, number) < Array.IndexOf(update,rule[Math.Abs(numberPos-1)]))
+                    {
+                        ruleViolated = true;
+                    }
+                }
                 
+                    
+            }
+        }
+
+        if (ruleViolated)
+        {
+            break;
+        }
+    }
+
+    if (ruleViolated)
+    {
+        ruleViolated = false;
+        int[] newUpdate = GetSortedUpdate(RelevantRules(rules, update),new List<int>());
+        result += newUpdate[(newUpdate.Length-1)/2];
+    }
+}
+
+Console.WriteLine(result);
+
+static List<int[]> RelevantRules(List<int[]> rules, int[] update)
+{
+    var result = new List<int[]>();
+    foreach (var number in update)
+    {
+        foreach (var rule in rules)
+        {
+            var numberPos = Array.IndexOf(rule, number);
+            if (numberPos != -1 && update.Contains(rule[Math.Abs(numberPos - 1)]))
+            {
+
+                result.Add(rule);
+
             }
         }
     }
+
+    return result.Distinct().ToList();
+
+}
+
+static int GetMostLeftNumber(List<int[]> list)
+{
+    int result = list.First().First();
+    int index = 0;
+    while(index < list.Count)
+    {
+        if (list[index][1] == result)
+        {
+            result = list[index][0];
+            index = 0;
+        }
+        index++;
+    }
+    return result;
+    
+}
+
+static int[] GetSortedUpdate(List<int[]> rules, List<int> result)
+{
+    var leftNumber = GetMostLeftNumber(rules);
+    result.Add(leftNumber);
+    rules = rules.Where(r => r[0] != leftNumber).ToList();
+    if (rules.Count > 1)
+    {
+        result = GetSortedUpdate(rules, result).ToList();
+    }
+
+    if (rules.Count == 1)
+    {
+        result.Add(rules[0][0]);
+        result.Add(rules[0][1]);
+        
+    }
+    return result.ToArray();
+   
 }
