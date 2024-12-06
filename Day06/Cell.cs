b2738778f26bff825a3c37bb65f2a906ded25991
Hendrik Denniger <hendrik.denniger@adesso.de>
2024-12-06 11:43:59 +0100

    wip

---
+++ Day06/Cell.cs
@@ -0,0 +1,77 @@
+namespace Day06;
+public enum FieldType
+{
+    Empty,
+    Obstacle,
+    Guard
+};
+public static class  EnumDirection
+{
+    public static readonly (int A, int B ) North = (-1,0);
+    public static readonly (int A, int B ) East = (0,1);
+    public static readonly (int A, int B ) South = (1,0);
+    public static readonly (int A, int B ) West = (0,-1);
+    
+};
+public class Cell
+{
+    
+    public bool Visited{get;set;}
+    public FieldType Type{get;set;}
+    public (int,int) Direction{get;set;}
+
+    public Cell(char field)
+    {
+        Visited = false;
+        switch (field)
+        
+        {
+         case '#':
+         {
+             Type = FieldType.Obstacle;
+             break;
+         } 
+         case '.':
+         {
+             Type = FieldType.Empty;
+             break;
+         }
+         case '^':
+         {
+             Type = FieldType.Guard;
+             Direction = EnumDirection.East;
+             Visited = true;
+             break;
+         }case '<':
+         {
+             Type = FieldType.Guard;
+             Direction = EnumDirection.West;
+             Visited = true;
+             break;
+         }case '>':
+         {
+             Type = FieldType.Guard;
+             Direction = EnumDirection.East;
+             Visited = true;
+             break;
+         }case 'v':
+         {
+             Type = FieldType.Guard;
+             Direction = EnumDirection.South;
+             Visited = true;
+             break;
+         }
+         default: throw new ArgumentException();
+        }
+    }
+
+    public void TurnRight()
+    {
+        if (this.Direction==EnumDirection.East) Direction = EnumDirection.South;
+        else if (this.Direction==EnumDirection.South) Direction = EnumDirection.West;
+        else if (this.Direction==EnumDirection.West) Direction = EnumDirection.North;
+        else if (this.Direction == EnumDirection.North) Direction = EnumDirection.East;
+    }
+
+    
+}
---
+++ Day06/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day06;
+
+string trial = "trial.txt";
+
+string input = "PuzzleInput06.txt";
+
+
+var inputBoard = LibraryTools.Tools.GetInput(trial);
+
+var board = inputBoard.Select(x => x.Select(y=>new Cell(y)).ToArray()).ToList(); 
+do
+{
+    try
+    {
+        var position = (from row in Enumerable.Range(0, board.Count)
+            from col in Enumerable.Range(0, board[row].Length)
+            where board[row][col].Type == FieldType.Guard
+            select (row, col)).First();
+        GuardStep(board, position);
+    }
+    catch (InvalidOperationException)
+    {
+        var result = board.Cast<Cell>().Where(c => c.Visited == true).Count();
+        Console.WriteLine(result);
+        break;
+    }
+} while (true);
+
+
+static List<Cell[]> GuardStep(List<Cell[]> board, (int row, int col) position)
+{
+    //var guard = board.FirstOrDefault(b=>b.FirstOrDefault(c=>c.)
+    
+    
+    var guard = board[position.row][position.col];
+    var nextPosition = (position.row + guard.Direction.Item1, position.col + guard.Direction.Item2);
+    if ( nextPosition.Item1 >= 0 && nextPosition.Item1 < board.Count && nextPosition.Item2 >= 0 && nextPosition.Item2 < board[nextPosition.Item1].Length)
+    {
+        while (board[nextPosition.Item1][nextPosition.Item2].Type ==
+            FieldType.Obstacle)
+        {
+            guard.TurnRight();
+            nextPosition = (position.row + guard.Direction.Item1, position.col + guard.Direction.Item2);
+        }
+        board[nextPosition.Item1][nextPosition.Item2] = guard;
+    }
+    board[position.row][position.col].Type = FieldType.Empty;
+    return board;
+}
